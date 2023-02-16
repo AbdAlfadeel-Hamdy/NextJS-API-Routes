a62d6eae1e316ab73e827135d6bfc6f2baff7380
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-16 04:19:12 +0200

    sending requests

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,20 @@ function HomePage() {
     e.preventDefault();
     const enteredEmail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback,
+    };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   };
   return (
     <div>
@@ -14,11 +28,16 @@ function HomePage() {
       <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your Feedback </label>
-          <textarea type="text" id="feedback" rows={5}></textarea>
+          <textarea
+            type="text"
+            id="feedback"
+            rows={5}
+            ref={feedbackInputRef}
+          ></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
