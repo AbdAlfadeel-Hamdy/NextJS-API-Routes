89da6f5521148eadfb7fff848651f0d26f3f5082
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-16 07:13:03 +0200

    Dynamic API Routes

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,27 @@
+import { Fragment, useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 const FeedbackPage = (props) => {
+  const [feedbackData, setFeedbackData] = useState();
+  const loadFeedbackHandler = (id) => {
+    fetch(`/api/${id}`)
+      .then((res) => res.json())
+      .then((data) => setFeedbackData(data.feedback));
+  };
   return (
-    <ul>
-      {props.feedbackItems.map((feedback) => (
-        <li key={feedback.id}>{feedback.text}</li>
-      ))}
-    </ul>
+    <Fragment>
+      <ul>
+        {props.feedbackItems.map((feedback) => (
+          <li key={feedback.id}>
+            {feedback.text}
+            <button onClick={loadFeedbackHandler.bind(null, feedback.id)}>
+              Show Setails
+            </button>
+          </li>
+        ))}
+      </ul>
+      {feedbackData && <p>{feedbackData.email}</p>}
+    </Fragment>
   );
 };
 
